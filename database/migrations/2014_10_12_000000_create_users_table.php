afb02f398ed992b4cc4625004e3abe378f3d7dbe
alfaqosry <81825300+alfaqosry@users.noreply.github.com>
2024-12-07 00:45:30 +0700

    update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('nik');
+            $table->string('no_hp');
+            $table->string('alamat');
+            $table->string('jenis_dagangan');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TokoController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,3 +22,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard.index');
 Route::get('/login', [AuthController::class, 'login'])->name('auth.login');
 Route::post('/login/post', [AuthController::class, 'post'])->name('auth.post');
+
+Route::get('/toko', [TokoController::class, 'index'])->name('toko.index');
+
